dd41826a92914f3f72e6b2d5fc5912c81a2dbe81
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-16 20:25:29 -0700

    Added models for Posts, Authors and Tags.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(
+        Author, on_delete=models.PROTECT, related_name="posts")
+    excerpt = models.TextField()
+    image = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag,  related_name="posts")
+    slug = models.SlugField(default="", null=False, db_index=True)
+
+    def __str__(self):
+        return f"{self.title}"
