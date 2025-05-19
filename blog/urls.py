0ce8d01549c9700e71ec72fd46e0e246e8a5d816
Ryan Van de Kamp <contact@ryanvandekamp.com>
2025-05-19 16:50:56 -0700

    Added comment functionality to posts.

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("", views.index, name="blog-index"),
-    path("posts", views.posts, name="blog-posts"),
-    path("posts/<slug:slug>", views.post, name="blog-post")
+    path("", views.IndexView.as_view(), name="blog-index"),
+    path("posts", views.PostsView.as_view(), name="blog-posts"),
+    path("posts/<slug:slug>", views.PostView.as_view(), name="blog-post")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,100 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Author, Tag
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .forms import CommentForm
 
 
-def index(request):
-    """
-    path: /
-    Returns the rendered index page.
-    """
-    # [:3] Python slicing syntax This line gets the 3 newest posts.
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+class IndexView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    # This property sets ordering, list ordering methods.
+    odering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        data = query_set[:3]
+        return data
 
 
-def posts(request):
-    """
-    path: /posts
-    Returns the rendered posts list page.
-    """
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/posts.html", {
-        "posts": all_posts
-    })
+# def index(request):
+#     """
+#     path: /
+#     Returns the rendered index page.
+#     """
+#     # [:3] Python slicing syntax This line gets the 3 newest posts.
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     return render(request, "blog/index.html", {
+#         "posts": latest_posts
+#     })
+
+class PostsView(ListView):
+    template_name = "blog/posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+# def posts(request):
+#     """
+#     path: /posts
+#     Returns the rendered posts list page.
+#     """
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/posts.html", {
+#         "posts": all_posts
+#     })
 
 
-def post(request, slug):
-    """
-    path: /posts/slug
-    Returns the rendered post page.
-    """
-    post_data = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/single-post.html", {
-        "post": post_data,
-        "tags": post_data.tags.all()
-    })
+class PostView(View):
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        return render(request, "blog/single-post.html", {
+            "post": post,
+            "tags": post.tags.all(),
+            "comment_form": CommentForm()
+        })
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            # Must re-add a reference to the post on submit.
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("blog-post", args=[slug]))
+
+        return render(request, "blog/single-post.html", {
+            "post": post,
+            "tags": post.tags.all(),
+            "comment_form": CommentForm()
+        })
+
+# class PostView(DetailView):
+#     template_name = "blog/single-post.html"
+#     model = Post
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["tags"] = self.object.tags.all()
+#         context["comment_form"] = CommentForm()
+#         return context
+
+# def post(request, slug):
+#     """
+#     path: /posts/slug
+#     Returns the rendered post page.
+#     """
+#     post_data = get_object_or_404(Post, slug=slug)
+#     return render(request, "blog/single-post.html", {
+#         "post": post_data,
+#         "tags": post_data.tags.all()
+#     })
